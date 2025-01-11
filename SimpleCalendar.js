c061ba7c707d67918c6c63d933444b7ecafee3ea
Matthew Pouliotte <poul102@gmail.com>
2025-01-11 09:12:37 -0500

    I modernized the string building.

--- SimpleCalendar.js
+++ SimpleCalendar.js
@@ -56,50 +56,50 @@ function buildCalendar(month, year) {
     var today = new Date();
     today = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1);
     
-    htmlToAdd += "<tr class=\"calendar-day-of-week-header\">";
+    htmlToAdd += `<tr class="calendar-day-of-week-header">`;
     for(i =0 ; i < 7; i++)
     {
-        var cssClass = (i == 0 || i  == 6) ? "calendar-day-weekend" : "";
-        htmlToAdd += "<td class=\"" + cssClass + "\">" + daysOfWeek[i] + "</td>";
+        var cssClass = (i == 0 || i  == 6) ? `calendar-day-weekend` : ``;
+        htmlToAdd += `<td class="${cssClass}">${daysOfWeek[i]}</td>`;
     }
 
     for (var i = 0; daysInMonth >= dayOfMonthCounter && i < 6; i++) {
-        htmlToAdd += "<tr class=\"calendar-day-header\">";
+        htmlToAdd += `<tr class="calendar-day-header">`;
         for (var dayCounter = 0; dayCounter < 7; dayCounter++) {
             var dayAdding = new Date(year, month, (dayOfMonthCounter - 1));
 
             if ((dayOfMonthCounter > 1 && daysInMonth >= dayOfMonthCounter) ||
                 (dayOfMonthCounter === 1 && monthStartDayOfWeek <= dayCounter)) {
                 if (dayCounter === 0 || dayCounter === 6) {
-                    cssTagsForDaysOfWeek[dayCounter] = "calendar-day-weekend";
+                    cssTagsForDaysOfWeek[dayCounter] = `calendar-day-weekend`;
                 } else {
-                    cssTagsForDaysOfWeek[dayCounter] = "";
+                    cssTagsForDaysOfWeek[dayCounter] = ``;
                 }
 
                 if (today.getTime() === dayAdding.getTime()) {
-                    cssTagsForDaysOfWeek[dayCounter] = cssTagsForDaysOfWeek[dayCounter] + " calendar-day-current-day";
+                    cssTagsForDaysOfWeek[dayCounter] = cssTagsForDaysOfWeek[dayCounter] + ` calendar-day-current-day`;
                 }
 
-                htmlToAdd += "<td class=\"" + cssTagsForDaysOfWeek[dayCounter] + "\">" + dayOfMonthCounter + "</td>";
+                htmlToAdd += `<td class="${cssTagsForDaysOfWeek[dayCounter]}">${dayOfMonthCounter}</td>`;
 
-                cssTagsForDaysOfWeek[dayCounter] = cssTagsForDaysOfWeek[dayCounter] + " calendar-day-in-month";
-                dataDatesForDaysOfWeek[dayCounter] = (month + 1) + "/" + dayOfMonthCounter + "/" + year;
+                cssTagsForDaysOfWeek[dayCounter] = cssTagsForDaysOfWeek[dayCounter] + ` calendar-day-in-month`;
+                dataDatesForDaysOfWeek[dayCounter] = (month + 1) + `/` + dayOfMonthCounter + `/` + year;
                 dayOfMonthCounter++;
             } else {
-                htmlToAdd += "<td class=\"calendar-day-other-month\">&nbsp</td>";
-                cssTagsForDaysOfWeek[dayCounter] = "calendar-day-other-month";
-                dataDatesForDaysOfWeek[dayCounter] = "";
+                htmlToAdd += `<td class="calendar-day-other-month">&nbsp</td>`;
+                cssTagsForDaysOfWeek[dayCounter] = `calendar-day-other-month`;
+                dataDatesForDaysOfWeek[dayCounter] = ``;
             }
 
         }
-        htmlToAdd += "</tr><tr class=\"calendar-day-cell\">";
+        htmlToAdd += `</tr><tr class="calendar-day-cell">`;
         for (var dayCounter2 = 0; dayCounter2 < 7; dayCounter2++) {
-            htmlToAdd += "<td data-date=\"" + dataDatesForDaysOfWeek[dayCounter2] + "\" class=\"" + cssTagsForDaysOfWeek[dayCounter2] + "\"></td>";
+            htmlToAdd += `<td data-date="${dataDatesForDaysOfWeek[dayCounter2]}" class="${cssTagsForDaysOfWeek[dayCounter2]}"></td>`;
         }
-        htmlToAdd += "</tr>";
+        htmlToAdd += `</tr>`;
     }
 
-    htmlToAdd = "<table class=\"calendar-table\">" + htmlToAdd + "</table>";
+    htmlToAdd = `<table class="calendar-table">${htmlToAdd}</table>`;
 
     //todo the ID should be passed in.
     document.getElementById(itemToBindTo).innerHTML = htmlToAdd;
@@ -112,10 +112,10 @@ function getHeaderPart(month, year) {
     firstOfMonth = new Date(year,  month, 1)
     monthTitle = firstOfMonth.toLocaleString('default', { month: 'long'});
 
-    var htmlToAdd = "<div><h3 class=\"calendar-header-title\">" + monthTitle + " " + year + "</h3>";
-    htmlToAdd += "<div class=\"calendar-header-buttons\"><button id=\"calendar-lastMonthButton\" class=\"btn btn-primary\">&lt;</button>&nbsp;";
-    htmlToAdd += "<button id=\"calendar-nextMonthButton\" class=\"btn btn-primary\">&gt;</button></div>";
-    htmlToAdd += "<div class=\"calendar-header-buttons\"><button  id=\"calendar-currnetMonthButton\" class=\"btn btn-primary\">Today</buutton></div></div>";
+    var htmlToAdd = `<div><h3 class="calendar-header-title">${monthTitle} ${year}</h3>`;
+    htmlToAdd += `<div class="calendar-header-buttons"><button id="calendar-lastMonthButton" class="btn btn-primary">&lt;</button>&nbsp;`;
+    htmlToAdd += `<button id="calendar-nextMonthButton" class="btn btn-primary">&gt;</button></div>`;
+    htmlToAdd += `<div class="calendar-header-buttons"><button  id="calendar-currnetMonthButton" class="btn btn-primary">Today</buutton></div></div>`;
     return htmlToAdd;
 }
 
@@ -129,7 +129,7 @@ function getDaysOfWeek() {
 }
 
 function getStartOfMonth(month, year) {
-    var d = new Date((month + 1) + "/1/" + year);
+    var d = new Date((month + 1) + `/1/` + year);
     var dayOfWeek = d.getDay();
     return dayOfWeek;
 }
